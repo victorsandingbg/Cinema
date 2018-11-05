5a37cf54db697049d0f43fd9a9baf2e67d66a788
Marie-Claire <havasoa87@gmail.com>
2018-11-05 23:37:43 +0100

    Fixade till resenär sidan av programmet. Nu fixar BussLineMeny hanteringen av all data genom att skapa och ha hand om respektive linje objekt.

--- PROGRAM_MASTER.py
+++ PROGRAM_MASTER.py
@@ -1,9 +1,50 @@
-from class5 import *
+from human.StaffManager import StaffManager
+from buss.BussLineMenu import BussLineMenu
+
+
+class Menu:
+    def __init__(self):
+        self.choices = {
+            "1": self.company,
+            "2": self.consumer,
+            "3": False
+        }
+
+        self.staffManager = StaffManager()
+        self.bussLineMenu = BussLineMenu()
+
+    def display_menu(self):
+        print("""
+        Main Menu
+        1. Are you Company?
+        2. Or are you a Consumer?
+        3. Quit""")
+
+    def run(self):
+        runProgram = True
+        while runProgram:
+            self.display_menu()
+            choice = input("Enter an option: ")
+            action = self.choices.get(choice)
+            if action is False:
+                runProgram = False
+            elif action:
+                action()
+            else:
+                print("{0} is not a valid choice".format(choice))
+
+    def company(self):
+        password = input("Please insert password")
+        if password == "dog":
+            self.staffManager.run()
+        else:
+            print("Wrong password")
+
+    def consumer(self):
+        self.bussLineMenu.run()
 
 
 def main():
-    print("Här ska vi köra programmet")
-
     menu = Menu()
     menu.run()
 
--- class5.py
+++ class5.py
@@ -1,6 +1,5 @@
-from human.StaffManager import StaffManager
-from buss.BussLineMenu import BussLineMenu
-
+## Denna måste göras om?...
+'''
 class TrafficMenu:
     def __init__(self):
         self.choices = {
@@ -28,41 +27,4 @@ Trafikcentral menyn
                 print(f"""Choice of driver: {Bussdriver.printname(action)}""")
             else:
                 print("is not an option".format(choice))
-
-
-class Menu:
-    def __init__(self):
-        self.choices = {
-            "1": self.company,
-            "2": self.consumer,
-        }
-
-        self.staffManager = StaffManager()
-#        self.bussLineMenu = BussLineMenu()
-
-    def display_menu(self):
-        print("""
-        Main Menu
-        1. Are you Company?
-        2. Or are you a Consumer?
-        """)
-
-    def run(self):
-        while True:
-            self.display_menu()
-            choice = input("Enter an option: ")
-            action = self.choices.get(choice)
-            if action:
-                action()
-            else:
-                print("{0} is not a valid choice".format(choice))
-
-    def company(self):
-        password = input("Please insert password")
-        if password == "dog":
-            self.staffManager.run()
-        else:
-            print("Wrong password")
-
-    def consumer(self):
-        self.bussLineMenu.run()
+'''
